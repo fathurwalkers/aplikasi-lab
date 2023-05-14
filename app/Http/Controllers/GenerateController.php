c70bfe394b5e4229e01b8f6e1f51fe325873a3e0
Fathur Walkers <fathurwalkers44@gmail.com>
2023-05-14 13:59:54 -0700

    update

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -19,6 +19,109 @@ class GenerateController extends Controller
 {
     public function generate_pengguna()
     {
-        //
+        $faker = Faker::create('id_ID');
+
+        $jenis_kelamin = ["L", "P"];
+
+        for ($i=0; $i < 20; $i++) {
+            // DATA
+            $data_nama = $faker->name();
+            $data_telepon = $faker->phoneNumber();
+            $data_jeniskelamin = Arr::random($jenis_kelamin);
+            $data_kode = strtoupper(Str::random(8));
+
+            // LOGIN
+            $login_model            = new Login;
+            $password               = '12345';
+            $hashPassword           = Hash::make($password, [
+                'rounds' => 12,
+            ]);
+            $token_raw              = Str::random(16);
+            $token                  = Hash::make($token_raw, [
+                'rounds' => 12,
+            ]);
+            $level                  = "user";
+            $login_status           = "verified";
+            $random_email           = "mail" . strtolower(Str::random(5)) . "@gmail.com";
+            $login_data = $login_model->create([
+                'login_nama'        => $data_nama,
+                'login_username'    => 'user' . $i . strtolower(Str::random(5)),
+                'login_password'    => $hashPassword,
+                'login_email'       => $random_email,
+                'login_telepon'     => $data_telepon,
+                'login_token'       => $token,
+                'login_level'       => $level,
+                'login_status'      => $login_status,
+                'created_at'        => now(),
+                'updated_at'        => now()
+            ]);
+            $login_data->save();
+
+            $data = new Data;
+            $save_data = $data->create([
+                "data_nama" => $data_nama,
+                "data_telepon" => $data_telepon,
+                "data_jeniskelamin" => $data_jeniskelamin,
+                "data_kode" => $data_kode,
+                "created_at" => now(),
+                "updated_at" => now()
+            ]);
+            $save_data->save();
+            $save_data->login()->associate($login_data->id);
+            $save_data->save();
+        }
+    }
+
+    public function generate_default_pengguna()
+    {
+        $faker = Faker::create('id_ID');
+
+        $jenis_kelamin = ["L", "P"];
+
+        // DATA
+        $data_nama = $faker->name();
+        $data_telepon = $faker->phoneNumber();
+        $data_jeniskelamin = Arr::random($jenis_kelamin);
+        $data_kode = strtoupper(Str::random(8));
+
+        // LOGIN
+        $login_model            = new Login;
+        $password               = '12345';
+        $hashPassword           = Hash::make($password, [
+            'rounds' => 12,
+        ]);
+        $token_raw              = Str::random(16);
+        $token                  = Hash::make($token_raw, [
+            'rounds' => 12,
+        ]);
+        $level                  = "user";
+        $login_status           = "verified";
+        $random_email           = "mail" . strtolower(Str::random(5)) . "@gmail.com";
+        $login_data = $login_model->create([
+            'login_nama'        => $data_nama,
+            'login_username'    => "example",
+            'login_password'    => $hashPassword,
+            'login_email'       => $random_email,
+            'login_telepon'     => $data_telepon,
+            'login_token'       => $token,
+            'login_level'       => $level,
+            'login_status'      => $login_status,
+            'created_at'        => now(),
+            'updated_at'        => now()
+        ]);
+        $login_data->save();
+
+        $data = new Data;
+        $save_data = $data->create([
+            "data_nama" => $data_nama,
+            "data_telepon" => $data_telepon,
+            "data_jeniskelamin" => $data_jeniskelamin,
+            "data_kode" => $data_kode,
+            "created_at" => now(),
+            "updated_at" => now()
+        ]);
+        $save_data->save();
+        $save_data->login()->associate($login_data->id);
+        $save_data->save();
     }
 }
